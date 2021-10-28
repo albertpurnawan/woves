1ca320083af02660d27e42b8f608987ad9d54659
unknown <albertpurnawan1@gmail.com>
2021-10-28 23:26:25 +0700

    first commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
-import Logout from './Logout';
+import Logout from "./Logout";
 
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
   return (
     <nav>
-      <h1>Waves</h1>
-      <button data-testid="libraryBtn" onClick={() => setLibraryStatus(!libraryStatus)}>
+      <h1>Woves</h1>
+      <button
+        data-testid="libraryBtn"
+        onClick={() => setLibraryStatus(!libraryStatus)}
+      >
         Library
         <FontAwesomeIcon icon={faMusic} />
       </button>
